5476fb3e5f0e0e03847117d49eadf09d64881c64
Sergiychuk Viachaslau <qertysig@gmail.com>
2025-07-28 11:25:17 +0300

    Add serializator

---
+++ Runtime/Utils/Data/DataSerializer.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Utils.Data
+{
+    public static class DataSerializer
+    {
+        public static byte[] Serialize(object obj)
+        {
+            using var memoryStream = new MemoryStream();
+            
+            new BinaryFormatter().Serialize(memoryStream,obj);
+            
+            return memoryStream.ToArray();
+        }
+
+        public static T Deserialize<T>(byte[] bytes)
+        {
+            using var memoryStream = new MemoryStream(bytes);
+            return (T)new BinaryFormatter().Deserialize(memoryStream);
+        }
+    }
+}
